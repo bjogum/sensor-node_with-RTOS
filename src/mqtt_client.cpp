3665a41d86b878c270b8a367bf1b7c4cb643fb8d
bjogum <bjogum@gmail.com>
2026-03-05 11:52:55 +0100

    upd mqtt

--- src/mqtt_client.cpp
+++ src/mqtt_client.cpp
@@ -1,3 +1,13 @@
 #include "alarm.h"
 
-// 
+// -- avgör om datan behöver publiseras - Beroende på sensorer/status samt state --
+// a) skicka all data vid "init" (även RSSI)
+// b) skicka förändrad data || var 30e sek
+// c) skicka heartbeat var 30e sek (bra trots LWT)
+// d) LWT: Maxtid, 10s offline -> ESP ger larm 
+
+
+    // Om: SKICKA datan (pub)
+
+
+// TA EMOT data (sub) från ESP -> "state"
---
+++ src/wifi_manager.cpp
@@ -0,0 +1,7 @@
+#include <WiFi3.h>
+
+void maintainWiFi(){
+    // kolla om vi är anslutan till wifi
+    // om inte, försök ansluta
+    // sätt 'wifiConnected' variabel till = false
+}
